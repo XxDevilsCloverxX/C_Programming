f179c6d5b05d1964508bc62a7dd705fa5bc62d03
XxDevilsCloverxX <silasrodriguez02@gmail.com>
2022-05-11 22:10:23 -0500

    directory update via bash

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m "file update via bash" && git branch -M main && git push -u origin main
+git add . && git commit -m "directory update via bash" && git branch -M main && git push -u origin main
